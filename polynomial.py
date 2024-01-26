eeacaa7ce9765a3803866541082deec072addbac
Your Name <your_email@example.com>
2024-01-26 18:18:30 -0500

    cs 506 exercise part c

--- polynomial.py
+++ polynomial.py
@@ -4,6 +4,9 @@ class X:
 
     def __repr__(self):
         return "X"
+    
+    def evaluate(self, xVal):
+        return xVal
 
 class Int:
     def __init__(self, i):
@@ -11,6 +14,9 @@ class Int:
     
     def __repr__(self):
         return str(self.i)
+    
+    def evaluate(self, xVal):
+        return self.i
 
 class Add:
     def __init__(self, p1, p2):
@@ -20,12 +26,17 @@ class Add:
     def __repr__(self):
         return repr(self.p1) + " + " + repr(self.p2)
     
+    def evaluate(self, xVal):
+        return self.p1.evaluate(xVal) + self.p2.evaluate(xVal)
+
 class Sub:
     def __init__(self, p1, p2):
         self.p1 = p1
         self.p2 = p2
     def __repr__(self):
         return repr(self.p1) + " - " + repr(self.p2)
+    def evaluate(self, xVal):
+        return self.p1.evaluate(xVal) - self.p2.evaluate(xVal)
 
 class Mul:
     def __init__(self, p1, p2):
@@ -41,6 +52,9 @@ class Mul:
             return repr(self.p1) + " * ( " + repr(self.p2) + " )"
         return repr(self.p1) + " * " + repr(self.p2)
 
+    def evaluate(self, xVal):
+        return self.p1.evaluate(xVal) * self.p2.evaluate(xVal)
+    
 class Div:
     def __init__(self, p1, p2):
         self.p1 = p1
@@ -57,6 +71,11 @@ class Div:
             return repr(self.p1) + " / ( " + repr(self.p2) + " )"
         return repr(self.p1) + " / " + repr(self.p2)
 
+    def evaluate(self, xVal):
+        if self.p2.evaluate(xVal) == 0:
+            raise ZeroDivisionError("Division by zero will cause error")
+        return self.p1.evaluate(xVal) / self.p2.evaluate(xVal)
+
 
 poly = Add( Add( Int(4), Int(3)), Add( X(), Mul( Int(1), Add( Mul(X(), X()), Int(1)))))
-print(poly)
+print(poly.evaluate(-1))
